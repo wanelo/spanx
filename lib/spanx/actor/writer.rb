82805ccdda545327b440337c5b68f23185c266f8
Konstantin Gredeskoul <kigster@gmail.com>
2012-10-22 19:13:55 -0700

    Adding a test for run_command

--- lib/spanx/actor/writer.rb
+++ lib/spanx/actor/writer.rb
@@ -39,21 +39,23 @@ module Spanx
               end
               contents_now = File.read(@block_file)
               if contents_now != contents_previous && @run_command
-                if @run_command
-                  result = system(@run_command)
-                  if result
-                    Logger.log "run command [#{@run_command}] executed successfully"
-                  elsif result == false
-                    Logger.log "run command [#{@run_command}] returned non-zero exit status"
-                  elsif result.nil?
-                    Logger.log "run command [#{@run_command}] failed -- #{$_}"
-                  end
-                end
+                run_command()
               end
             rescue Exception => e
               error_exit_with_msg "ERROR writing to block file #{@block_file} or running command: #{e.inspect}"
             end
           end
+        end
+      end
+
+      def run_command
+        result = system(@run_command)
+        if result
+          Logger.log "run command [#{@run_command}] executed successfully"
+        elsif result == false
+          Logger.log "run command [#{@run_command}] returned non-zero exit status"
+        elsif result.nil?
+          Logger.log "run command [#{@run_command}] failed -- #{$_}"
         end
       end
     end
--- spec/spanx/actor/writer_spec.rb
+++ spec/spanx/actor/writer_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 require 'fileutils'
+require 'tempfile'
 
 describe Spanx::Actor::Writer do
   before do
     Spanx.stub(:redis).and_return(Redis.new)
   end
 
-  describe "#write" do
 
+  describe "#write" do
     let(:config) { {
       block_file: "/tmp/block_file.#{$$}",
     }}
-
-    let(:adapter) { mock(blocked_ips: ["1.2.3.4", "127.0.0.1"])}
     let(:writer) { Spanx::Actor::Writer.new(config, adapter)}
-
+    let(:adapter) { mock(blocked_ips: ["1.2.3.4", "127.0.0.1"])}
     after do
       ::FileUtils.rm(config[:block_file])
     end
@@ -24,5 +23,20 @@ describe Spanx::Actor::Writer do
       contents = File.read(config[:block_file])
       contents.should == "deny 1.2.3.4;\ndeny 127.0.0.1;\n"
     end
+  end
+
+  describe "#run_command" do
+    let(:tempfile) { Tempfile.new("output")}
+    let(:adapter) { mock() }
+    let(:config) { {
+      run_command: "echo 'OK' >> #{tempfile.path}"
+    }}
+    let(:writer) { Spanx::Actor::Writer.new(config, adapter)}
+    it "properly runs command" do
+      writer.run_command
+      contents = File.read(tempfile.path)
+      contents.should eql("OK\n")
+    end
+
   end
 end
