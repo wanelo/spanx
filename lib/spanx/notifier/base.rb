9a8c23817ba1b398747c5591e14313cab645e13f
James Hart & Richard Millan <james+richard@wanelo.com>
2015-07-23 13:43:35 -0700

    Return error message when `host` command is not found in PATH

--- lib/spanx/notifier/base.rb
+++ lib/spanx/notifier/base.rb
@@ -10,18 +10,19 @@ module Spanx
 
       protected
 
-        def generate_block_ip_message(blocked_ip)
-          violated_period = blocked_ip.period_check
-          "#{blocked_ip.identifier} blocked @ #{Time.at(blocked_ip.timestamp)} " \
-            "for #{violated_period.block_ttl/60}mins, for #{blocked_ip.sum} requests over " \
-            "#{violated_period.period_seconds/60}mins, with #{violated_period.max_allowed} allowed. " \
-            "Host: #{host(blocked_ip.identifier)}"
-        end
+      def generate_block_ip_message(blocked_ip)
+        violated_period = blocked_ip.period_check
+        "#{blocked_ip.identifier} blocked @ #{Time.at(blocked_ip.timestamp)} " \
+          "for #{violated_period.block_ttl/60}mins, for #{blocked_ip.sum} requests over " \
+          "#{violated_period.period_seconds/60}mins, with #{violated_period.max_allowed} allowed. " \
+          "Host: #{host(blocked_ip.identifier)}"
+      end
 
-        def host(ip)
-          `host #{ip}`
-        end
-
+      def host(ip)
+        %x(host #{ip})
+      rescue Errno::ENOENT
+        'Could not find host command. Make sure it is included in your PATH.'
+      end
     end
   end
 end
